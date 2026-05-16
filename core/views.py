79e22831c2fe3187641b56f80f33f31fcba4e99b
Priya <priya@bashup.in>
2026-05-16 15:34:44 +0530

    fix NameError: import User in views

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
